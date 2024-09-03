bd033221d64de8baef60457339ff091e545af6a0
Caio Oliveira <caio.oliveira07@gmail.com>
2024-09-03 17:05:02 -0300

    Descrição da home atualizada

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ function App() {
   return (
     <>
      <h1>Home Page</h1>
-     {/* Exibe o header e a lista de todos os filmes disponíveis. */}
+     {/* Exibe o header e a lista de filmes disponível divididos por
+     Antigos(antes dos anos 2000)
+     Bem-avaliados(nota maior que 9)
+     Para você(sorteie 5 filmes aleatórios da lista*/}
     </>
   )
 }
