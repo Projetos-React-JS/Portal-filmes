a1214bead5f12f28addbb29d82d8924bde779e9c
Caio Oliveira <caio.oliveira07@gmail.com>
2024-09-03 17:48:01 -0300

    Main.jsx com instruções atualizadas

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+{/* Deve ter as rotas:
+  home
+  lista de filmes(ela será o conteúdo da home)
+  gêneros
+  filmes por gênero
+  detalhes do filme
+  */}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
