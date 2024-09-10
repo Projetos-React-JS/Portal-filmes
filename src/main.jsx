d6227c5124c483cf9140fd10a07ebb402a8bba78
ViniciusVilasB <boas.vini@gmail.com>
2024-09-10 20:58:51 -0300

    Feito em aula

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,34 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
+import MoviesByGenrePage from './pages/MoviesByGenre.jsx'
+import MovieListPage from './pages/MovieList.jsx'
+import MovieDetailPage from './pages/MovieDetail.jsx'
+import Home from './pages/Home.jsx'
+import GenreList from './pages/GenreList.jsx'
+import PageNotFound from './pages/PageNotFound.jsx'
 
-{/* Deve ter as rotas:
-  home
-  filmes
-  Detalhes do Filme
-  Lista de gêneros
-  Filmes por gênero
-  Page Not Found
-  */}
-
+const router= createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {index: true, element: <Home />},
+      {path: '/movies', element: <MovieListPage />},
+      {path: '/movies/:id', element: <MovieDetailPage />},
+      {path: '/genre', element: <GenreList />},
+      {path: '/genre/:id', element: <MoviesByGenrePage />},
+      {path: '*', element: <PageNotFound/>}
+    ]
+    
+  }
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
+
+// https://github.com/Projetos-React-JS/Portal-filmes/tree/Site-construido/src
