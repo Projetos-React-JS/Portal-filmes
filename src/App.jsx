723c1342ad73ce518ba6b1791c10bd9c56b69c50
Caio Oliveira <caio.oliveira07@gmail.com>
2024-09-17 17:38:03 -0300

    Ajustes em Props e comentários removidos

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,6 @@ function App() {
     <>
      <Header/>
      <Outlet/>
-     {/* Exibe o header e a lista de filmes disponível divididos por
-     Antigos(antes dos anos 2000)
-     Bem-avaliados(nota maior que 9)
-     Para você(sorteie 5 filmes aleatórios da lista*/}
     </>
   )
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,13 +1,13 @@
 import { Link } from "react-router-dom";
 
-export default function MovieCard(movie) {
+export default function MovieCard({id, titulo, imagem_destaque}){
 
     return(
         <>
-        <div key={movie.id} className="flex flex-col justify-center items-center">
-            <h2>{movie.titulo}</h2>
-              <img src={movie.imagem_destaque} alt={movie.titulo} className="w-28 h-36"/>
-              <Link to={`/movies/${movie.id}`}>Ver detalhes</Link>
+        <div key={id} className="flex flex-col justify-center items-center">
+            <h2>{titulo}</h2>
+              <img src={`/${imagem_destaque}`} alt={titulo} className="w-28 h-36"/>
+              <Link to={`/movies/${id}`}>Ver detalhes</Link>
         </div>
         </>
     )
