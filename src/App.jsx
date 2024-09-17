88a30d1c28d91ad7bb25be0842768a75b6c6fe42
Caio Oliveira <caio.oliveira07@gmail.com>
2024-09-17 19:48:33 -0300

    Atualizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+import { Outlet } from "react-router-dom"
+import Header from "./components/Header"
+
 function App() {
 
   return (
     <>
-     <h1>Home Page</h1>
-     {/* Exibe o header e a lista de filmes disponível divididos por
-     Antigos(antes dos anos 2000)
-     Bem-avaliados(nota maior que 9)*/}
+      <Header />
+      <Outlet />
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,32 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+import GenreList from './pages/GenreListPage.jsx'
+import Home from './pages/Home.jsx'
+import MovieDetailPage from './pages/MovieDetailPage.jsx'
+import MovieListPage from './pages/MovieListPage.jsx'
+import MoviesByGenrePage from './pages/MoviesByGenrePage.jsx'
+import PageNotFound from './pages/PageNotFound.jsx'
 
-{/* Deve ter as rotas:
-  home
-  filmes
-  Detalhes do Filme
-  Lista de gêneros
-  Filmes por gênero
-  Page Not Found
-  */}
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: '/movies', element: <MovieListPage /> },
+      { path: '/movies/:id', element: <MovieDetailPage /> },
+      { path: '/genre', element: <GenreList /> },
+      { path: '/genre/:id', element: <MoviesByGenrePage /> },
+      { path: '*', element: <PageNotFound /> }
+    ]
 
+  }
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
