6dd27fe6e52e2cb0c5b58133629a772cddbf67e5
Caio Oliveira <caio.oliveira07@gmail.com>
2024-09-17 23:50:06 -0300

    Ajuste Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,12 @@ import { NavLink } from "react-router-dom";
 export default function Header() {
     return (
         <>
-            <header className="w-full h-10 flex justify-between items-center p-10 mb-10 bg-purple-900">
+            <header>
                 <div>
                     <h1>Portal Filmes</h1>
                 </div>
                 <nav>
-                    <ul className="flex gap-5">
+                    <ul>
                         <li><NavLink to="/">Home</NavLink></li>
                         <li><NavLink to="/movies">Filmes</NavLink></li>
                         <li><NavLink to="/genre">Gêneros</NavLink></li>
