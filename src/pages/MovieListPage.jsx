d373eed26c36fc2efe1bf53e4884662caf701eba
Ryan Brito <rm554497@fiap.com.br>
2024-09-25 21:00:44 -0300

    Aula Use State

--- src/pages/MovieListPage.jsx
+++ src/pages/MovieListPage.jsx
@@ -1,10 +1,38 @@
-export default function MovieListPage(){
-    return(
-        <>
-        <h1>Movie List Page</h1>
-        {/* Exibe a lista de todos os filmes disponíveis categorizados por gênero.
-        Cada filme na lista é mostrado com uma imagem, 
-        título e um link para a página de detalhes do filme.*/}
-        </>
-    )
+import { useState } from "react";
+import movies from "../data/movies.json";
+import MovieCard from "../components/MovieCard";
+
+export default function MovieListPage() {
+    const [search, setSearch] = useState("");
+
+    const handleSearch = (e) => {
+        setSearch(e.target.value);
+        console.log(search);
+    };
+
+    const filmesFiltrados = movies.filter((filme) =>
+        filme.titulo.toLowerCase().includes(search.toLocaleLowerCase())
+    );
+
+    return (
+        <div className="min-h-scree flex flex-col items-center p-5">
+            <h1 className="text-3xl font-bold mb-4">Veja o catálogo completo de filmes</h1>
+            <input
+                className="w-full  text-black max-w-md p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-200"
+                type="text"
+                name="search"
+                id="search"
+                value={search}
+                onChange={handleSearch}
+                placeholder="Buscar filmes..."
+            />
+            <section className="mt-5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-5">
+                {filmesFiltrados.length > 0 ?
+                    filmesFiltrados.map((filme) => (
+                    <MovieCard key={filme.id} {...filme} />
+                )):
+                <p>Não existem filmes na busca</p>}
+            </section>
+        </div>
+    );
 }
