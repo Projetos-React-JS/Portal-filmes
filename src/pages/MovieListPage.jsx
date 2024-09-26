2e5832d80c123b7d6fd20b2f601c1cafcfdfd61e
Caio Oliveira <caio.oliveira07@gmail.com>
2024-09-25 21:07:45 -0300

    Update MovieListPage.jsx

--- src/pages/MovieListPage.jsx
+++ src/pages/MovieListPage.jsx
@@ -1,45 +1,35 @@
 import { useState } from 'react';
 import MovieCard from '../components/MovieCard';
-import data from '../data/movies.json';
+import movies from '../data/movies.json';
 
-export default function MovieListPage(){
+export default function MovieListPage() {
 
-    const [search, setSearch] = useState('');
-    const [filteredMovies, setFilteredMovies] = useState(data);
-
-    function handleClick(){
-
-      const filmesFiltrados = data.filter(movie => ( movie.titulo.toLowerCase().includes(search.toLowerCase()) ));
-    
-      setFilteredMovies(filmesFiltrados);
-    }
+  const [search, setSearch] = useState('');
 
 
-    return(
-        <>
-        <main className='grid grid-cols-4 justify-items-center gap-5'>
-          <h2 className='col-span-4 text-2xl font-bold text-center mb-5'>Veja o catálogo completo de filmes</h2>
-          <form className='col-span-4 w-2/4 flex flex-col justify-center gap-2 mb-5'>
-            <input 
-              type='text' 
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-              placeholder='Pesquise um filme...' 
-              className=' text-black p-2 mb-5 border border-gray-300 rounded-md' />
-            <button 
-              type='submit'
-              onClick={handleClick}
-              className=' bg-purple-800 text-white p-2 rounded-md'>
-                Pesquisar
-            </button>
-          </form>
-          {
-            filteredMovies.map((movie) => (
-                <MovieCard key={movie.id} {...movie} />
-              )
-            )
-          }
-        </main>
-        </>
-    )
+  const filmesFiltrados = movies.filter(movie => (movie.titulo.toLowerCase().includes(search.toLowerCase())));
+
+
+
+  return (
+    <>
+      <main className='grid grid-cols-4 justify-items-center gap-5'>
+        <h2 className='col-span-4 text-2xl font-bold text-center mb-5'>Veja o catálogo completo de filmes</h2>
+        <form className='col-span-4 w-2/4 flex flex-col justify-center gap-2 mb-5'>
+          <input
+            type='text'
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder='Pesquise um filme...'
+            className=' text-black p-2 mb-5 border border-gray-300 rounded-md' />
+        </form>
+        {
+          filmesFiltrados.map((movie) => (
+            <MovieCard key={movie.id} {...movie} />
+          )
+          )
+        }
+      </main>
+    </>
+  )
 }
