90f33d2f36986c4f98fc08429b25337dbd60243f
Caio Oliveira <caio.oliveira07@gmail.com>
2024-09-27 22:44:12 -0300

    Atualizado

--- src/pages/MovieListPage.jsx
+++ src/pages/MovieListPage.jsx
@@ -1,10 +1,40 @@
-export default function MovieListPage(){
-    return(
+import { useState } from "react"
+import MovieCard from "../components/MovieCard"
+import movies from "../data/movies.json"
+
+export default function MovieListPage() {
+
+    const [search, setSearch] = useState("")
+
+    const handleSearch = (event) => {
+        setSearch(event.target.value)
+        console.log(search)
+    }
+
+    const filmesFiltrados = movies.filter( filme => filme.titulo.toLowerCase().includes(search.toLowerCase()))
+
+    return (
         <>
-        <h1>Movie List Page</h1>
-        {/* Exibe a lista de todos os filmes disponíveis categorizados por gênero.
-        Cada filme na lista é mostrado com uma imagem, 
-        título e um link para a página de detalhes do filme.*/}
+            <h2>Veja o catálogo completo de filmes</h2>
+            <input
+                className="text-black"
+                type="text"
+                id="search"
+                value={search}
+                onChange={handleSearch}
+            />
+            <section className="flex">
+                {
+                    filmesFiltrados.length > 0 ?
+                    filmesFiltrados
+                        .map(filme => (
+                            <MovieCard key={filme.id} {...filme} />
+                        ))
+                        :
+                        <p>Filme não encontrado</p>
+                }
+            </section>
         </>
     )
 }
+
