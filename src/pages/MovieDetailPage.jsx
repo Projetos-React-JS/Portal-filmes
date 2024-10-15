fc99697b6f251f4698b4b8bb03c12878841a863b
Caio Oliveira <caio.oliveira07@gmail.com>
2024-10-15 11:59:36 -0300

    Página de detalhes do filme funcionando

--- src/pages/MovieDetailPage.jsx
+++ src/pages/MovieDetailPage.jsx
@@ -1,20 +1,26 @@
 import { useParams } from "react-router-dom"
-import data from '../data/movies.json'
+import { useEffect, useState } from "react";
 
 export default function MovieDetailPage(){
     
     const { id } = useParams()
+    const [movie, setMovie] = useState({})
 
-    const movie = data.find((movie) => movie.id == id)
+    useEffect(() => {
+        fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=7c572a9f5b3ba776080330d23bb76e1e&language=pt-BR`)
+        .then(response => response.json())
+        .then(data => {setMovie(data)})
+        .catch(err => console.error(err));
+    }, []);
     return(
         <>
         {
             movie ?
                 <div key={movie.id}>
-                    <h2>{movie.titulo}</h2>
-                    <img src={`/${movie.imagem_destaque}`} alt={movie.titulo} />
-                    <p>{movie.sinopse}</p>
-                    <p>{movie.diretor}</p>
+                    <h2>{movie.titulo || movie.title}</h2>
+                    <img src={movie.poster_path ? `https://image.tmdb.org/t/p/w154${movie.poster_path}` : `/${movie.imagem_destaque}`} alt={movie.title || movie.titulo} />
+                    <p>{movie.overview}</p>
+                    <p>{movie.vote_average}</p>
                 </div>
             : <p>Filme não encontrado</p>
         }
