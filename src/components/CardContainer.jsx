719e5965f0916b5497d110110d76f60bdd07d766
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-21 20:00:45 -0300

    zero

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,11 +1,12 @@
 export default function CardContainer({ titulo, children }) {
 
     return(
-        
+        <>
             <h1>{titulo}</h1>
             <div className="flex">
                 {children}
             </div>
-        </div>
+        
+        </>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,28 @@
+import { useEffect, useState } from "react";
 import CardContainer from "../components/CardContainer";
 
 import MovieCard from "../components/MovieCard";
 import movies from "../data/movies.json";
 export default function Home() {
+    const [filmes, setFilmes] = useState([])
+    useEffect(()=>{
+        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=7c572a9f5b3ba776080330d23bb76e1e&language=pt-br`)
+        .then(response => response.json())
+        .then(dados => setFilmes(dados))
+        .catch(error=> console.error(error))
+        .finally(console.log("fim do fetch"))        
+    },[])
 
+    const filmePorAno =(filme)=>{
+        let dataLancamento = filme.release_date
+        dataLancamento.split("-")
+        const anoLancamento = dataLancamento[0]
+        if(anoLancamento<2000){
+            return filme
+        }else{
+            return null
+        }
+    }
     return (
         <>
             <CardContainer titulo="Filmes antigos">
